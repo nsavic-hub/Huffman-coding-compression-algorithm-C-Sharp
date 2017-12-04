c79ecdf80d83b41cbb4e2a6d3908b7c8bcebd331
nsavichub <34226731+nsavichub@users.noreply.github.com>
2017-12-04 18:33:36 +1100

    Add files via upload

---
+++ DataStructures_Algorithms/Project1/Task01/DataSerializer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Algorithms.Project1
+{
+    public class DataSerializer<T> where T : IConvertible
+    {
+        public static void Serialise(string path, Vector<T> vector)
+        {
+            using (Stream stream = File.Open(path, FileMode.Create))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                bin.Serialize(stream, vector);
+            }
+        }
+        public void deserialise(string path, ref Vector<T> vector)
+        {
+
+            using (Stream stream = File.Open(path, FileMode.Open))
+            {
+                BinaryFormatter bin = new BinaryFormatter();
+                vector = (Vector<T>)bin.Deserialize(stream);
+
+            }
+        }
+
+        /* 
+        Loads the file
+        Reads all the bytes and then converts it tobase64 because streamreader and binaryreader alone can't read ascii values
+        Alternatively you could use "new StreamReader(path, Encoding.GetEncoding("iso-8859-1"), true))" 
+        */
+        public static void LoadFile(string path, ref Vector<T> vector)
+        {
+            vector = new Vector<T>();
+            var bytes = File.ReadAllBytes(path);
+            string output = Convert.ToBase64String(bytes);
+
+            foreach (var value in output)
+            {
+                vector.Add((T)Convert.ChangeType(value, typeof(T)));
+            }
+        }
+
+        /*
+        Uses a foreach loop to read every value in Vector
+        Adds them to a string and writes the string to the file
+        Alternatively you could use 'string.Join("", vector);' in place of a foreach loop
+        */
+        public static void SaveFile(string path, Vector<T> vector)
+        {
+            string line = "";
+            foreach (var vect in vector)
+            {
+                if(vect.ToString() != "\0")
+                    line += vect;
+            }
+
+            byte[] output = Convert.FromBase64String(line);
+            File.WriteAllBytes(path, output);
+        }
+
+        /*
+        Reads every line in the encodedinput text file.
+        */
+        public static void LoadVectorFromTextFile(string path, ref Vector<T> vector)
+        {
+            vector = new Vector<T>();
+            string line = "";
+
+            using (StreamReader sr = new StreamReader(File.Open(path, FileMode.Open)))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == " ")
+                    {
+                        vector.Add((T)Convert.ChangeType('\0', typeof(T)));
+                        continue;
+                    }
+                    vector.Add((T)Convert.ChangeType(line, typeof(T)));
+                }
+            }
+        }
+
+        /*
+        Saves to decoded_input.txt
+        */
+        public static void SaveVectorToTextFile(string path, Vector<T> vector)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.ASCII, 0x10000))
+            {
+                foreach (var vect in vector)
+                {
+                    sw.WriteLine(vect);
+                }
+            }
+        }
+
+    }
+}
---
+++ DataStructures_Algorithms/Project2/HuffmanCoding.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures_Algorithms.Project1;
+using System.IO;
+
+namespace DataStructures_Algorithms.Project2
+{
+    public class Node
+    {
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public char Symbol { get; set; }
+        public int Frequency { get; set; }
+    }
+
+    public class HuffmanCoding
+    {
+        private List<Node> listOfNodes = new List<Node>();
+        private Node Root { get; set; }
+
+
+        Node Add(Node left, Node right)
+        {
+            // Combines the two nodes together into one with a symbol of '₸', indicating that it's a parent node
+            left = new Node
+            {
+                Symbol = '₸',
+                Frequency = left.Frequency + right.Frequency,
+                LeftChild = left,
+                RightChild = right
+            };
+
+            return left;
+        }
+        private void Build(Vector<char> input)
+        {
+            // Creates a list of nodes that holds the values of Char and Int as symbol and frequency
+            foreach (var i in input)
+            {
+
+                foreach (var node in listOfNodes)
+                {
+                    // If it's an empty line, break
+                    if (i == '\0')
+                        break;
+
+                    // If the node already exists append the frequency
+                    if (node.Symbol == i)
+                    {
+                        node.Frequency++;
+                        break;
+                    }
+
+                    // If were at the end of the list and the symbol doesn't already exist then create a new node
+                    if (listOfNodes.IndexOf(node) == listOfNodes.Count() - 1)
+                    {
+                        listOfNodes.Add(new Node
+                        {
+                            Symbol = i,
+                            Frequency = 1,
+                            LeftChild = null,
+                            RightChild = null
+                        });
+                        break;
+                    }
+                }
+
+                // If we are at the beginning of the list, create a node
+                if (listOfNodes.Count == 0)
+                {
+                    listOfNodes.Add(new Node
+                    {
+                        Symbol = i,
+                        Frequency = 1,
+                        LeftChild = null,
+                        RightChild = null
+                    });
+                }
+            }
+        }
+
+        public string binary = "";
+        private void Traverse(Node node, TextWriter tw)
+        {
+            if (node == null)
+                return;
+
+            if (node.LeftChild != null)
+            {
+                binary += "0";
+
+                // If the left child is not a parent node add to a list of binarys
+                // along with its symbol and how many times it has been traversed
+                if (node.LeftChild.Symbol != '₸')
+                    binaryFreq.Add(node.LeftChild.Symbol, binary);
+
+                Traverse(node.LeftChild, tw);
+                binary = binary.Remove(binary.Length - 1);
+            }
+
+
+            if (node.RightChild != null)
+            {
+                binary += "1";
+
+                if (node.RightChild.Symbol != '₸')
+                    binaryFreq.Add(node.RightChild.Symbol, binary);
+
+                Traverse(node.RightChild, tw);
+                binary = binary.Remove(binary.Length - 1);
+            }
+        }
+
+        public Vector<string> Encode(Vector<char> input)
+        {
+            Build(input);
+
+            /* 
+                1. Find the 2 lowest values and remove them from frequency
+                2. Add() function is executed sending the nodes with the lowest value
+                3. Add the newly created node to the list
+            */
+
+
+            while (listOfNodes.Count() != 1)
+            {
+                var lowest = listOfNodes.Aggregate((l, r) => l.Frequency < r.Frequency ? l : r);
+                listOfNodes.Remove(lowest);
+
+                var secondLowest = listOfNodes.Aggregate((l, r) => l.Frequency < r.Frequency ? l : r);
+                listOfNodes.Remove(secondLowest);
+
+                Root = Add(lowest, secondLowest);
+                listOfNodes.Add(Root);    // Adds the current node to the list
+            }
+
+            Traverse(Root, Console.Out);
+
+
+            /*
+                Read every char in the input and if it matches the binary of the traversed node, add it a list
+            */
+            Vector<string> encoded = new Vector<string>(input.Count);
+            foreach (var value in input)
+            {
+                foreach (var str in binaryFreq)
+                {
+                    if (str.Key == value)
+                        encoded.Add(str.Value);
+                }
+            }
+
+            return encoded;
+        }
+
+        public Vector<char> Decode(Vector<string> input)
+        {
+            var result = new Vector<char>();
+
+            string inUsage = "";
+            foreach (var inp in input)
+            {
+                inUsage += inp;
+
+                foreach (var str in binaryFreq)
+                {
+                    if (str.Value == inUsage)
+                    {
+                        inUsage = "";
+                        result.Add(str.Key);
+                        continue;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<char, string> _BinaryFreq = new Dictionary<char, string>();
+        public Dictionary<char, string> binaryFreq
+        {
+            get { return _BinaryFreq; }
+            set { _BinaryFreq = value; }
+        }
+    }
+}
